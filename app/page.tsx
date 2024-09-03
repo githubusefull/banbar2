55d4984fe43e7980aaf411a1877b4628aea0c3a5
“benbaringenierieetconseil“ <benbaringenierieetconseil@gmail.com>
2024-09-03 15:47:12 +0100

    ok fien to banbar repo

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,21 @@
 import Image from "next/image";
 
 export default function Home() {
+  const newLocal = <a
+    className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+    href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    By{" "}
+    <Image
+      src="/vercel.svg"
+      alt="Vercel Logo"
+      className="dark:invert"
+      width={100}
+      height={24}
+      priority />
+  </a>;
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
@@ -9,22 +24,7 @@ export default function Home() {
           <code className="font-mono font-bold">app/page.tsx</code>
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+          {newLocal}
         </div>
       </div>
 
@@ -33,7 +33,7 @@ export default function Home() {
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
           src="/next.svg"
           alt="Next.js Logo"
-          width={180}
+          width={1111}
           height={37}
           priority
         />
@@ -110,4 +110,6 @@ export default function Home() {
       </div>
     </main>
   );
+
+
 }
